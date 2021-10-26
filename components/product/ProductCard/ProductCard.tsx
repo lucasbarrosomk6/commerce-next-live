a3c7fcb6ddda8ccca0b17cad114077a9976172a1
lucasbarrosomk6 <53588870+lucasbarrosomk6@users.noreply.github.com>
2021-10-26 10:34:17 -0700

    now with free sandwiches

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -43,7 +43,7 @@ const ProductCard: FC<Props> = ({
         {variant === 'slim' && (
           <>
             <div className={s.header}>
-              <span>{product.name}</span>
+              <span>{product.name} witha free sandwich</span>
             </div>
             {product?.images && (
               <Image
@@ -55,6 +55,7 @@ const ProductCard: FC<Props> = ({
                 layout="fixed"
                 {...imgProps}
               />
+              stuff
             )}
           </>
         )}
